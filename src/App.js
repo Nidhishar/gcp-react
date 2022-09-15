a364c12ec2d2b1d2a0d1a97e5c09f554861995ba
Nidhi <nidhi.sharma@clixlogix.net>
2022-09-15 17:41:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edited / change and save to reload.</p>
-        <h4>This is the test of GCP deployment</h4>
         <h6>Author: Nidhi</h6>
       </header>
     </div>
